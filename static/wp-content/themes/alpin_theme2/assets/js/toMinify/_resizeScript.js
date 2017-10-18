57fad308e5828b562c4a19cd2f3597413131bc3b
szucst <t.szucs@plan-net.com>
2017-10-18 14:55:49 +0300

    shadow up and down + resize js modified + contact map

--- static/wp-content/themes/alpin_theme2/assets/js/toMinify/_resizeScript.js
+++ static/wp-content/themes/alpin_theme2/assets/js/toMinify/_resizeScript.js
@@ -2,16 +2,18 @@ var resizeScreen = (function(){
 
     function bindEvents(){
         var headerH = $("#site-header-main").outerHeight();
-        console.log(headerH);
         var footerH = $("footer").outerHeight();
-        console.log(footerH);
         var imgH = $(".block-picture").outerHeight();
-        console.log(imgH);
+        var map = $("#map").outerHeight();
         var viewportH = $(window).height();
-        console.log(viewportH);
 
         if(imgH != undefined){
             var middleH = viewportH - (headerH + footerH) - imgH;
+            console.log(middleH);
+            $(".middle").height(middleH);
+        }
+        else if(map != undefined){
+            var middleH = viewportH - (headerH + footerH) - map;
             console.log(middleH);
             $(".middle").height(middleH);
         }
--- static/wp-content/themes/alpin_theme2/page-templates/page-contact.php
+++ static/wp-content/themes/alpin_theme2/page-templates/page-contact.php
@@ -35,6 +35,23 @@ get_header();
 				<div class="clear"></div>
 			</div>
 		</div>
+		<div id="map"></div>
+		<script>
+            function initMap() {
+                var uluru = {lat: 45.764480, lng: 21.214600};
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 14,
+                    center: uluru
+                });
+                var marker = new google.maps.Marker({
+                    position: uluru,
+                    map: map
+                });
+            }
+		</script>
+		<script async defer
+				src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDYsxhkhe-gVDeehqme-BihX-wJiO19kxQ&callback=initMap">
+		</script>
 	</div>
 </section>
 
